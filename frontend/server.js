c313c80aa0e002cefdc1c35b464513b6469edaef
Marcelo Leal <marcelocardosoleal@gmail.com>
2025-12-22 19:56:49 -0300

    Fix: Infraestrutura Docker dinâmica

--- frontend/server.js
+++ frontend/server.js
@@ -4,21 +4,10 @@ const app = express();
 
 const { REACT_APP_BACKEND_URL, REACT_APP_HOURS_CLOSE_TICKETS_AUTO } = process.env;
 
-app.use((req, res, next) => {
-  res.set("Cache-Control", "no-store, no-cache, must-revalidate, proxy-revalidate, max-age=0");
-  res.set("Pragma", "no-cache");
-  res.set("Expires", "0");
-  next();
-});
-
-app.use(express.static(path.join(__dirname, "build"), { etag: false, maxAge: 0 }));
+app.use(express.static(path.join(__dirname, "build")));
 
 app.get("/*", function (req, res) {
   res.sendFile(path.join(__dirname, "build", "index.html"));
-});
-
-app.get("/health", (req, res) => {
-  res.json({ ok: true });
 });
 
 app.listen(3250, () => {
